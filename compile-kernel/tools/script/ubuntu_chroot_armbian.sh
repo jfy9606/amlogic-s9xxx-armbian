08c8d5cb9c5acf6179a2eb69627025b1af175821
jfy <jfy9606@qq.com>
2026-03-12 09:24:20 +0000

    add ubuntu_chroot_armbian.sh

---
+++ compile-kernel/tools/script/ubuntu_chroot_armbian.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#==================================================================================
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+# This file is a part of the Rebuild Armbian
+# https://github.com/jfy9606/amlogic-s9xxx-armbian
+#
+# Description: Generate uInitrd in chroot environment
+#
+#==================================================================================
+
+kernel_outname="${1}"
+
+echo -e "Start generating uInitrd for [ ${kernel_outname} ] in chroot environment..."
+
+# Set compress format
+compress_format="xz"
+compress_initrd_file="/etc/initramfs-tools/initramfs.conf"
+if [[ -f "${compress_initrd_file}" ]]; then
+    sed -i "s|^COMPRESS=.*|COMPRESS=${compress_format}|g" ${compress_initrd_file}
+    echo -e "Set compress format to [ ${compress_format} ]"
+fi
+
+# Enable update_initramfs
+initramfs_conf="/etc/initramfs-tools/update-initramfs.conf"
+[[ -f "${initramfs_conf}" ]] && sed -i "s|^update_initramfs=.*|update_initramfs=yes|g" ${initramfs_conf}
+
+# Generate initrd.img and uInitrd
+cd /boot
+update-initramfs -c -k ${kernel_outname}
+
+# Disable update_initramfs
+[[ -f "${initramfs_conf}" ]] && sed -i "s|^update_initramfs=.*|update_initramfs=no|g" ${initramfs_conf}
+
+# Create uInitrd
+if [[ -f "initrd.img-${kernel_outname}" ]]; then
+    mkimage -A arm64 -O linux -T ramdisk -C gzip -n uInitrd -d initrd.img-${kernel_outname} uInitrd-${kernel_outname}
+    echo -e "Successfully created uInitrd-${kernel_outname}"
+else
+    echo -e "ERROR: initrd.img-${kernel_outname} not found"
+    exit 1
+fi
+
+# Create header package
+echo -e "Creating header package..."
+mkdir -p /opt/header
+cd /usr/src/linux-headers-${kernel_outname}
+tar -czf /opt/header/header-${kernel_outname}.tar.gz .
+
+echo -e "All done in chroot environment."
